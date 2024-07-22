73fd0438b7492ebf32b6445f8f6dfe54ef368444
nata <nata.cercvadze1@gmail.com>
2024-07-22 14:58:54 +0400

    mentors section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+// mentors-slider
+document.addEventListener('DOMContentLoaded', function() {
+  const mentorsSlider = document.querySelector('.mentors-slider');
+    const slider = document.querySelector('.slider');
+    const mentorsBox = document.querySelectorAll('.person-box');
+    const slideHeight = mentorsBox[0].offsetHeight; 
+
+    let slideIndex = 0
+
+    const moveToNextSlide = () => {
+      slideIndex++
+      if (slideIndex >= mentorsBox.length) {
+        slideIndex = 0
+      }
+      const translateY = -slideIndex * slideHeight
+      slider.style.transform = `translateY(${translateY}px)`
+    }
+
+    const slideInterval = setInterval(moveToNextSlide, 2000)
+    
+
+    mentorsSlider.addEventListener('mouseout', () => {
+      clearInterval(slideInterval);
+  });
+
+  mentorsSlider.addEventListener('mouseleave', () => {
+    slideInterval = setInterval(moveToNextSlide, 2000);
+});
+})
+
+
 // accordion
 document.addEventListener("DOMContentLoaded", () => {
   const headers = document.querySelectorAll(".accordion-header");
