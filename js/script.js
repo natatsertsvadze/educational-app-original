b9a25c4f29e23dff88c507dbd7417559512c205a
Nancy Kikvadze <137420286+lost-in-noise@users.noreply.github.com>
2024-07-26 22:37:03 +0400

    Revert "fix: carousel"

--- js/script.js
+++ js/script.js
@@ -17,40 +17,16 @@ document.addEventListener("DOMContentLoaded", function () {
     slider.style.transform = `translateY(${translateY}px)`;
   };
 
+  const slideInterval = setInterval(moveToNextSlide, 2000);
 
-let currentMentorsIndex = 0;
-const boxPerSlide = 3;
+  mentorsSlider.addEventListener("mouseout", () => {
+    clearInterval(slideInterval);
+  });
 
-const slides = document.querySelector(".slider");
-const boxes = Array.from(slides.children);
-const boxesSlides = boxes.length;
-const totalSlides = Math.ceil(boxesSlides / boxPerSlide);
-
-boxes.forEach(box => {
-  const clone = box.cloneNode(true);
-  slides.appendChild(clone);
+  mentorsSlider.addEventListener("mouseleave", () => {
+    slideInterval = setInterval(moveToNextSlide, 2000);
+  });
 });
-
-function toTheNextSlide() {
-  const slideHeight = slides.firstElementChild.getBoundingClientRect().height;
-  currentMentorsIndex++;
-  if (currentMentorsIndex >= boxesSlides) {
-    currentMentorsIndex = 0;
-    slides.style.transition = 'none'; // Disable transition when jumping back to start
-    slides.style.transform = 'translateY(0)';
-    
-    // Force reflow to restart the animation
-    slides.offsetHeight; // Trigger reflow
-    
-    // Re-enable transition and move to the next slide
-    slides.style.transition = 'transform 0.5s ease';
-  }
-  
-  const offset = -currentMentorsIndex * slideHeight;
-  slides.style.transform = `translateY(${offset}px)`;
-}
-
-setInterval(toTheNextSlide, 3000);
 
 // new courses favourite
 const fullHeart = document.querySelectorAll(".new-courses-emptyheart");
